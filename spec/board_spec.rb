42b64ed67494d359b037405945077f798748160a
Kevin Newland <kevin.newland@yahoo.com>
2024-10-25 10:57:02 -0500

    write tests for board #cells
    Co-authored-by: Dustin <dpeukert04@gmail.com>

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,4 +7,26 @@ RSpec.describe Board do
       expect(board).to be_an_instance_of(Board)
     end
   end
+
+  describe "#cells" do
+    it "is a hash" do
+      board = Board.new
+      board.cells
+      expect(board.cells).to be_a(Hash)
+    end
+
+    it "should have 16 key/value pairs" do
+      board = Board.new
+      board.cells
+      expect(board.cells.size).to eq(16)
+    end
+
+    it "has keys that point to cell objects" do
+      board = Board.new
+      board.cells
+      board.cells.each do |cell|
+        expect(cell).to be_a(Cell)
+      end
+    end
+  end
 end
