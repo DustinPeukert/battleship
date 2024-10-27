a720e7d1eb42cc6d92d0ad875cad04fdb94717c5
Dustin Peukert <dpeukert04@gmail.com>
2024-10-27 14:40:40 -0400

    fix typos

--- spec/cpu_spec.rb
+++ spec/cpu_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe CPU do
   describe '#initialize' do
     it 'exists' do
       cpu_board = Board.new
-      cpu = CPU.new(cpu_boardboard)
+      cpu = CPU.new(cpu_board)
 
       expect(cpu).to be_an_instance_of(CPU)
     end
 
     it 'has attributes' do
       cpu_board = Board.new
-      cpu = CPU.new(cpu_boardboard)
+      cpu = CPU.new(cpu_board)
 
       expect(cpu.board).to eq(cpu_board)
       expect(cpu.board).to be an be_an_instance_of(Board)
