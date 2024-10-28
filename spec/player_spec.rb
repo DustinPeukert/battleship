0447ff528518d4ae2e9e90040b6cd437c43e5ff2
Kevin Newland <kevin.newland@yahoo.com>
2024-10-28 17:08:33 -0500

    write test for fire at method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,6 +3,7 @@ require_relative 'spec_helper'
 RSpec.describe Player do
   describe '#initialize' do
     it 'exists' do
+      player_board = Board.new
       player = Player.new(player_board)
       expect(player).to be_an_instance_of(Player)
     end
@@ -14,7 +15,7 @@ RSpec.describe Player do
       expect(player.board).to eq(player_board)
     end
   end
-  
+
   context 'it can use the #place_ship which is already tested' do 
     it 'can place ship' do
       player_board = Board.new
@@ -23,5 +24,17 @@ RSpec.describe Player do
       expect(player_board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to be true
       expect(player.place(cruiser, ["A1", "A2", "A3"])).to be true
     end
+  end
+
+  describe '#fire_at' do
+    it 'player can fire at oppenents board' do
+      player_board = Board.new
+      player = Player.new(player_board)
+      opponent_board = Board.new
+      coordinate = "A1"
+      player.fire_at(opponent_board, coordinate)
+
+      expect(opponent_board.cells["A1"].fired_upon?).to be true 
+    end   
   end
 end
