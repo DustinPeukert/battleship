e6985171a31b46535918583cb32f0802cfd315d2
Kevin Newland <kevin.newland@yahoo.com>
2024-10-28 18:10:02 -0500

    add method fire at

--- lib/player.rb
+++ lib/player.rb
@@ -8,4 +8,8 @@ class Player
   def place(ship, coordinates)
    @board.place(ship, coordinates)
   end
+
+  def fire_at(opponent_board, coordinate)
+    opponent_board.cells[coordinate].fire_upon
+  end
 end
